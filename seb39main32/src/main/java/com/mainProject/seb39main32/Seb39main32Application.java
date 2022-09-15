370baa6c904e372bb518c8199812d091c04626c8
alrdlrsha1 <alrdlrsha1@naver.com>
2022-09-15 14:33:11 +0900

    first commit

---
+++ seb39main32/src/main/java/com/mainProject/seb39main32/Seb39main32Application.java
@@ -0,0 +1,13 @@
+package com.mainProject.seb39main32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb39main32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb39main32Application.class, args);
+	}
+
+}
