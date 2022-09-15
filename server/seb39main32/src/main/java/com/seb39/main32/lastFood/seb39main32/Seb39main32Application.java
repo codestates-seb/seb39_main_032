644398f81ed25de8559c67b7563234d01d74dc0b
JUNSUNGCONDING <hjs5038@gmail.com>
2022-09-15 13:49:57 +0900

    22-09-15 초기 셋팅

---
+++ server/seb39main32/src/main/java/com/seb39/main32/lastFood/seb39main32/Seb39main32Application.java
@@ -0,0 +1,13 @@
+package com.seb39.main32.lastFood.seb39main32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb39main32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb39main32Application.class, args);
+	}
+
+}
---
+++ server/seb39main32/src/test/java/com/seb39/main32/lastFood/seb39main32/Seb39main32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb39.main32.lastFood.seb39main32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb39main32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
