cd5f2833f0cd8232f5a729f0daa4b810d5b8c814
alrdlrsha1 <alrdlrsha1@naver.com>
2022-09-29 10:07:28 +0900

    fix : wish 외래키 적용 수정

--- seb39main32/src/main/java/com/mainProject/seb39main32/wish/service/WishService.java
+++ seb39main32/src/main/java/com/mainProject/seb39main32/wish/service/WishService.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Transactional
