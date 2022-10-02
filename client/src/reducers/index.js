557196a767835ba5b666f204ec1c44b97cc363f0
sheey37 <sheey37@gmail.com>
2022-10-02 22:22:44 +0900

    fix: reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import loginReducer from "./loginReducer";
 import userInfoReducer from "./userInfoReducer";
 import itemListReducer from "./itemListReducer";
+import itemReducer from "./itemReducer";
 
 const rootReducer = combineReducers({
   loginReducer,
   userInfoReducer,
   itemListReducer,
+  itemReducer,
 });
 
 export default rootReducer;
--- client/src/reducers/itemListReducer.js
+++ client/src/reducers/itemListReducer.js
@@ -4,7 +4,7 @@ import { SET_ITEMS_LIST, SET_FILTERED_ITEMS_LIST } from "../actions/index";
 //초기 상태 값
 const initialState = [];
 
-//login과 관련된 reducer(상태 관리) 함수
+//reducer(상태 관리) 함수
 const itemListReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_ITEMS_LIST:
